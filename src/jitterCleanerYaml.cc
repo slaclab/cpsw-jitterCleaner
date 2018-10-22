ab0696db0ac06d678b831a026136fb11297ac8bb
Kukhee Kim <khkim@lcls-dev3.slac.stanford.edu>
2018-10-22 13:00:29 -0700

    Initial commit for Jitter Cleaner API

---
+++ src/jitterCleanerYaml.cc
@@ -0,0 +1,163 @@
+#include <cpsw_api_builder.h>
+#include <cpsw_api_user.h>
+
+#include <jitterCleanerYaml.hh>
+
+#include <stdio.h>
+#include <string.h>
+
+using namespace JitterCleaner;
+
+JitterCleanerYaml::JitterCleanerYaml(Path path)
+{
+    _path = path;
+
+    _phaseTarget = IScalVal   ::create(_path->findByName("PhaseTarget"));
+    _delayValue  = IScalVal_RO::create(_path->findByName("DelayValue"));
+    _locked      = IScalVal_RO::create(_path->findByName("Locked"));
+    _phaseValid  = IScalVal_RO::create(_path->findByName("PhaseValid"));
+    _phaseCount  = IScalVal_RO::create(_path->findByName("PhaseCount"));
+    _scanState   = IScalVal_RO::create(_path->findByName("ScanState"));
+    _delaySet    = IScalVal_RO::create(_path->findByName("DelaySet"));
+    _scanCount   = IScalVal_RO::create(_path->findByName("ScanCount"));
+    _phaseCenter = IScalVal_RO::create(_path->findByName("PhaseCenter"));
+    _phaseCalc   = IScalVal_RO::create(_path->findByName("PhaseCalc"));
+    _reScan      = IScalVal  ::create(_path->findByName("ReScan"));
+
+   
+}
+
+
+void JitterCleanerYaml::setPhaseTarget(uint32_t target)
+{
+    _phaseTarget->setVal(&target);
+}
+
+void JitterCleanerYaml::setReScan(uint32_t rescan)
+{
+    _reScan->setVal(&rescan);
+}
+
+uint32_t JitterCleanerYaml::phaseTarget(void)
+{
+    uint32_t       val;
+    IndexRange     rng(0);
+
+    _phaseTarget->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+uint32_t JitterCleanerYaml::delayValue(void)
+{
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _delayValue->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+
+uint32_t JitterCleanerYaml::locked(void)
+{
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _locked->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+
+uint32_t JitterCleanerYaml::phaseValid(void)
+{
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _phaseValid->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+
+uint32_t JitterCleanerYaml::phaseCount(void)
+{
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _phaseCount->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+
+uint32_t JitterCleanerYaml::scanState(void)
+{
+
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _scanState->getVal(&val, 1, &rng);
+
+    return val;
+
+}
+
+
+uint32_t JitterCleanerYaml::delaySet(void)
+{
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _delaySet->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+uint32_t JitterCleanerYaml::scanCount(void)
+{
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _scanCount->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+
+uint32_t JitterCleanerYaml::phaseCenter(void)
+{
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _phaseCenter->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+
+uint32_t JitterCleanerYaml::phaseCalc(void)
+{
+    uint32_t        val;
+    IndexRange      rng(0);
+
+    _phaseCalc->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+
+
+uint32_t JitterCleanerYaml::reScan(void)
+{
+    uint32_t         val;
+    IndexRange       rng(0);
+
+
+    _reScan->getVal(&val, 1, &rng);
+
+    return val;
+}
+
+
---
+++ src/jitterCleanerYaml.hh
@@ -0,0 +1,50 @@
+#ifndef JITTER_CLEANER_YAML_HH
+#define JITTER_CLEANER_YAML_HH
+
+#include <cpsw_api_builder.h>
+
+#include <stdint.h>
+#include <vector>
+
+namespace JitterCleaner {
+
+    class JitterCleanerYaml {
+
+        protected:
+            Path          _path;
+
+            /* registers */
+            ScalVal       _phaseTarget;
+            ScalVal_RO    _delayValue;
+            ScalVal_RO    _locked;
+            ScalVal_RO    _phaseValid;
+            ScalVal_RO    _phaseCount;
+            ScalVal_RO    _scanState;
+            ScalVal_RO    _delaySet;
+            ScalVal_RO    _scanCount;
+            ScalVal_RO    _phaseCenter;
+            ScalVal_RO    _phaseCalc;
+            ScalVal       _reScan;
+
+        public:
+            JitterCleanerYaml(Path path);
+            void        setPhaseTarget(uint32_t target);
+            void        setReScan(uint32_t rescan);
+
+            uint32_t     phaseTarget(void);
+            uint32_t     delayValue(void);
+            uint32_t     locked(void);
+            uint32_t     phaseValid(void);
+            uint32_t     phaseCount(void);
+            uint32_t     scanState(void);
+            uint32_t     delaySet(void);
+            uint32_t     scanCount(void);
+            uint32_t     phaseCenter(void);
+            uint32_t     phaseCalc(void);
+            uint32_t     reScan(void);
+
+    };
+
+};
+
+#endif /* JITTER_CLEANER_YAML_HH */
